1062a4649175b981427c49b47cfb599da51851f2
Sırrı Kaan Oğuzkan <145159548+kaanoguzkan@users.noreply.github.com>
2026-02-19 13:16:24 +0300

    resume update

--- src/components/ResumeModal.jsx
+++ src/components/ResumeModal.jsx
@@ -3,7 +3,8 @@ import { useTranslation } from 'react-i18next';
 import { useResume } from '../context/ResumeContext';
 
 function ResumeModal() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const pdfUrl = `/assets/resume-${i18n.language}.pdf`;
   const { isOpen, close } = useResume();
 
   useEffect(() => {
@@ -33,9 +34,9 @@ function ResumeModal() {
             &times;
           </button>
         </div>
-        <iframe src="/assets/resume.pdf" title="Resume" className="resume-iframe" />
+        <iframe src={pdfUrl} title="Resume" className="resume-iframe" />
         <div className="resume-modal-footer">
-          <a href="/assets/resume.pdf" download className="btn">
+          <a href={pdfUrl} download className="btn">
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>
             {t('resume.download')}
           </a>
