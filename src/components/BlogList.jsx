d9d5f197b8bd50fc16714592eea53d0d54bb7aae
Sırrı Kaan Oğuzkan <145159548+kaanoguzkan@users.noreply.github.com>
2026-02-21 16:57:14 +0300

    fix 2

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -20,10 +20,6 @@ function BlogList() {
     <div className="blog-page">
       <div className="container">
         <div className="blog-header">
-          <Link to="/" className="blog-back-link">
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="19" y1="12" x2="5" y2="12"/><polyline points="12 19 5 12 12 5"/></svg>
-            {t('blog.backHome')}
-          </Link>
           <h1 className="section-title">{t('blog.title')}</h1>
           <p className="blog-subtitle">{t('blog.subtitle')}</p>
         </div>
